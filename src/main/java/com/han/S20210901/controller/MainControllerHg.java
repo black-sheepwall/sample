9b4cde555d50560895d0ae29d5d4721792821f3f
black-sheepwall <mrloveletter@naver.com>
2021-11-23 11:39:46 +0900

    2.branch Test

--- src/main/java/com/han/S20210901/controller/MainControllerHg.java
+++ src/main/java/com/han/S20210901/controller/MainControllerHg.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainControllerHg {
 	//Git hub test입니다. 
+	//Git hub Branch Test
 	@GetMapping(value="main")
 	public String main() {
 		
